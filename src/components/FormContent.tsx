5b9f7ff6a32930a12f468d46379a3d006b0cbd04
marriamdd <davitashvili.m3@gmail.com>
2024-04-11 14:06:34 +0400

    style form component

--- src/components/FormContent.tsx
+++ src/components/FormContent.tsx
@@ -1,30 +1,94 @@
 import styled from "styled-components";
 export default function FormContent() {
   return (
-    <div>
-      <form>
-        <div>
-          <label htmlFor="UserName">Cardholder Name</label>
-          <input id="userName" type="text" placeholder="e.g. Jane Appleseed" />
-        </div>
-        <div>
-          <label htmlFor="CardNumber">Card Number</label>
-          <input
-            id="CardNumber"
-            type="text"
-            placeholder="e.g. 1234 5678 9123 0000"
-          />
-        </div>
-        <div>
+    <FormContainer>
+      <PersonalInfoContainer>
+        <label htmlFor="UserName">Cardholder Name</label>
+        <input id="userName" type="text" placeholder="e.g. Jane Appleseed" />
+      </PersonalInfoContainer>
+      <PersonalInfoContainer>
+        <label htmlFor="CardNumber">Card Number</label>
+        <input
+          id="CardNumber"
+          type="text"
+          placeholder="e.g. 1234 5678 9123 0000"
+        />
+      </PersonalInfoContainer>
+      <AdditionalPersonInfo>
+        <div className="labelDiv">
           <label htmlFor="date">Exp. Date (MM/YY)</label>
-          <input id="date" type="text" placeholder="MM" />
+          <label htmlFor="cvc">CVC</label>
+        </div>
+        <div className="additionalInputDiv">
+          <input className="mm_yy" id="date" type="text" placeholder="MM" />
 
-          <input type="text" placeholder="YY" />
-          <label htmlFor="cvc"></label>
+          <input className="mm_yy" type="text" placeholder="YY" />
+
           <input id="cvc" type="text" placeholder="e.g. 123" />
         </div>
-        <button type="submit">Confirm</button>
-      </form>
-    </div>
+      </AdditionalPersonInfo>
+      <button type="submit">Confirm</button>
+    </FormContainer>
   );
 }
+
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10rem;
+  gap: 1.5rem;
+  label {
+    color: var(--Deep-Violet, #21092f);
+    font-size: 1.2rem;
+    font-weight: 500;
+    letter-spacing: 0.2rem;
+    text-transform: uppercase;
+  }
+  input {
+    height: 4rem;
+    border-radius: 0.8rem;
+    border: 1px solid var(--Light-Grey, #dfdee0);
+    background: var(--White, #fff);
+    padding-left: 2rem;
+  }
+  button {
+    width: 32.7rem;
+    height: 5.3rem;
+    border-radius: 0.8rem;
+    background: var(--Deep-Violet, #21092f);
+    color: var(--White, #fff);
+    font-size: 1.8rem;
+    font-weight: 500;
+    margin-top: 1rem;
+  }
+`;
+const PersonalInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  & > input {
+    width: 32.7rem;
+  }
+`;
+const AdditionalPersonInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  .labelDiv {
+    display: flex;
+    gap: 2rem;
+  }
+
+  .additionalInputDiv {
+    display: flex;
+    width: 32.7rem;
+    gap: 1rem;
+    .mm_yy {
+      width: 7.2rem;
+    }
+    #cvc {
+      width: 16.4rem;
+    }
+  }
+`;
--- src/globalStyles/GlobalStyles.tsx
+++ src/globalStyles/GlobalStyles.tsx
@@ -2,15 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap');
-  
+  *{
+    box-sizing: border-box;
+  }
     html{
-     
+    
         font-size: 62.5%;
     }
     body{
         margin: 0;
     padding: 0;
-        box-sizing: border-box;
+      
         background: var(--White, #FFF);
         font-family: "Space Grotesk", sans-serif;
         color: var(--White, #FFF);
